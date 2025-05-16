8ed9da78bbb42b8d133ecffec87f76e4d44a2fc9
ahmed <ahmednasserdts@gmail.com>
2025-05-16 20:19:03 +0300

    Init Hibernate Demo Project

---
+++ ORM/src/main/java/com/dizzy/App.java
@@ -0,0 +1,54 @@
+package com.dizzy;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+//            Student student_1 = new Student(100, "Ahmed", "ahmednaser@gmail.com","Student");
+//
+//            Configuration configuration = new Configuration();
+//
+//            SessionFactory sessionFactory = configuration.buildSessionFactory();
+//
+//            Session session = sessionFactory.openSession();
+//
+//            session.persist(student_1);
+
+//      JDBC
+//
+//        String sqlStatement = "Select name from products where product_id=1";
+//
+//        String username = "postgres";
+//        String password = "ahmednasser2003";
+//        String url = "jdbc:postgresql://localhost:5432/jdbc";
+//        Connection connection = DriverManager.getConnection( url,username,password);
+//
+//        Statement statement = connection.createStatement();
+//
+//        ResultSet resultSet = statement.executeQuery("Select name from products where product_id=1");
+//
+//        resultSet.next();
+//
+//        String name = resultSet.getString(1);
+//        System.out.println(name);
+//
+//        resultSet.close();
+//        connection.close();
+
+
+
+    }
+}
